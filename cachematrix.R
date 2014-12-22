e61249c8e92581ed5226d70f2ed9d5af5c29193f
psokhi <preetpal.sokhi@gmail.com>
2014-12-21 17:36:24 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+##HOW TO USE THE FUNCTIONS:
+## > test_matrix <- matrix(1:4,2,2)
+## > inv <- makeCacheMatrix()
+## > inv(test_matrix)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+makeCacheMatrix <- function(){
+  cachematrix <- matrix()
+  cacheresult <- matrix()
+  
+  cacheSolve <- function(arg_matrix) {
+    if (!(dim(cachematrix)==dim(arg_matrix)
+          && all(cachematrix==arg_matrix))){
+      print ("Solving new matrix")
+      inv <- solve(arg_matrix)
+      cachematrix <<- arg_matrix
+      cacheresult <<- inv
+    }else {print ("Using Cache")}
+    cacheresult
+  }
 }
